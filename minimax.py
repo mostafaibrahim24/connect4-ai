4956d28e5ce52a5bd16eae8d207339d01285897e
youssef1708 <youssefyasser2405@gmail.com>
2023-05-19 18:41:34 +0300

    add functions: -addconnect4piece -check_empty_location

--- minimax.py
+++ minimax.py
@@ -1,8 +1,18 @@
 import numpy as np
 
+
+R_count = 6
+
 def flip_mainBoard(mainBoard):
     # Initializing a numpy 2D array from the board
     mainBoard_copy = np.array(mainBoard)
     # Fliping the board
     mainBoard_copy = np.flip(mainBoard_copy, 0)
     return mainBoard_copy
+
+def addConnect4Piece(mainBoard, row, column, addedPiece):
+    mainBoard[row][column] = addedPiece
+
+def check_empty_location(mainBoard, column):
+    return mainBoard[R_count-1][column] == 0
+
