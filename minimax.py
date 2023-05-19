48e38eeea4dc9138b6b56bb6abe960a9c4307ecf
mostafaibrahim24 <mi.mostafaibrahim@gmail.com>
2023-05-19 19:08:31 +0300

    Add check winning state: horizontal, vertical

--- minimax.py
+++ minimax.py
@@ -2,6 +2,7 @@ import numpy as np
 
 
 R_count = 6
+c_count = 7
 
 def flip_mainBoard(mainBoard):
     # Initializing a numpy 2D array from the board
@@ -20,3 +21,15 @@ def get_first_empty_row(mainBoard, column):
     for row in range(R_count):
         if mainBoard[row][column] == 0:
             return row
+             
+def check_winning_state(mainBoard, checkedPiece):
+    for col in range(c_count-3):
+        for row in range(R_count):
+            if mainBoard[row][col] == checkedPiece\
+                    and mainBoard[row][col + 1] == checkedPiece and mainBoard[row][col + 2] == checkedPiece and mainBoard[row][col + 3] == checkedPiece:
+                return True
+
+    for col in range(c_count):
+        for row in range(R_count-3):
+            if mainBoard[row][col] == checkedPiece and mainBoard[row + 1][col] == checkedPiece and mainBoard[row + 2][col] == checkedPiece and mainBoard[row + 3][col] == checkedPiece:
+                return True
